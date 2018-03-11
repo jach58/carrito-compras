fbfb1fd471deee130bc336678abacb0e7ef705a6
Square <jach58@hotmail.com>
2018-03-11 00:33:33 -0600

    primer commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class UserController extends Controller
+{
+    public function getSignUp(){
+        return view('user.signup');
+    }
+
+    public function postSignUp(Request $request){
+        $this->validate($request,[
+            'email' => 'email|required|unique:users',
+            'password' => 'required|min:4'
+        ]);
+
+        $user = new User([
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+
+        $user->save();
+
+        Auth::login($user);
+
+        if(Session::has('oldUrl')){
+            $oldUrl = Session::get('oldUrl');
+            Session::forget('oldUrl');
+            return redirect()->to($oldUrl);
+        }
+
+        return redirect()->route('user.profile');
+
+    }
+
+    public function getSignIn(){
+        return view('user.signin');
+    }
+
+    public function postSignIn(Request $request){
+        $this->validate($request,[
+            'email' => 'email|required',
+            'password' => 'required|min:4'
+        ]);
+
+        if(Auth::attempt(['email'=>$request->input('email'), 'password'=>$request->input('password')])){
+            if(Session::has('oldUrl')){
+                $oldUrl = Session::get('oldUrl');
+                Session::forget('oldUrl');
+                return redirect()->to($oldUrl);
+            }
+            return redirect()->route('user.profile');
+        }
+        return redirect()->back();
+    }
+
+    public function getProfile(){
+        $orders = Auth::user()->orders;
+        $orders->transform(function($order,$key){
+            $order->cart = unserialize($order->cart);
+            return  $order;
+        });
+        return view('user.profile',['orders' => $orders]);
+    }
+
+    public function getLogout(){
+        Auth::Logout();
+        return redirect()->route('user.signin');
+    }
+}
---
+++ resources/views/shop/checkout.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Shopping Cart
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
+            <h1>Checkout</h1>
+            <h4> Your Total: ${{ $total }}</h4>
+            <div id="charge-error" class="alert alert-danger" {{!Session::has('error') ? 'hidden':''}}>
+                {{Session::get('error')}}
+            </div>
+            <form action="{{route('checkout')}}" method="post" id="checkout-form">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control" name="name" required>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input type="text" id="address" class="form-control" name="address" required>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label for="card-name">Card Holder Name</label>
+                            <input type="text" id="card-name" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label for="card-number">Credit Card Number</label>
+                            <div id="card-number" class="form-control"></div>
+                        </div>
+                    </div>
+                </div>
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-success">Buy now</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="{{URL::to('js/checkout.js')}}"></script>
+@endsection
